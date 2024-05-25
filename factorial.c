441fb26ed31fe05dd142e94b8579b9eb3b9b2cc9
jobayed003 <jobayed803@gmail.com>
2024-05-25 18:19:05 +0600

    solved problem four

---
+++ factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int n) {
+  if (n == 0)
+    return 1;
+  else
+    return n * factorial(n - 1);
+}
+
+int main() {
+  int n;
+  printf("Enter a non-negative integer: ");
+  scanf("%d", &n);
+
+  if (n < 0) {
+    printf("Factorial is not defined for negative integers.\n");
+  } else {
+    int fact = factorial(n);
+    printf("Factorial of %d is %d\n", n, fact);
+  }
+
+  return 0;
+}
